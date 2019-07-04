bd1674f2b6a685b4bf5297f9b1023334e96aca17
Sravani <sravani13200@gmail.com>
2019-07-04 22:56:11 +0530

    First commit
    Game related files including assets, prefabs and scripts.

---
+++ AdventureGame/Assets/Scripts/Weapons Script/ArrowBowScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowBowScript : MonoBehaviour
+{ 
+    private Rigidbody myBody;
+    public float speed = 30f;
+    public float deactivate_Timer = 3f;
+    public float damage = 50f;
+
+    private void Awake()
+    {
+        myBody = GetComponent<Rigidbody>();
+
+    }
+    void Start()
+    {
+        Invoke("DeactivateGameObject", deactivate_Timer);
+        
+    }
+
+    public void Launch(Camera mainCamera)
+    {
+        myBody.velocity = mainCamera.transform.forward * speed;
+
+
+        transform.LookAt(transform.position + myBody.velocity);
+
+    }
+    void DeactivateGameObject()
+    {
+        if (gameObject.activeInHierarchy)
+        {
+            gameObject.SetActive(false);
+        }
+       
+    }
+
+    private void OnTriggerEnter(Collider target)
+    {
+        //Debug.Log("Killed");
+        if (target.tag == Tags.ENEMY_TAG)
+        {
+            gameObject.SetActive(false);
+            Debug.Log("Killed enemy");
+        }
+
+    }
+
+}
+
